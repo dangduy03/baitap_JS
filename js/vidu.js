be3ad521b15cf8c250866153b6f5c309ed4db553
dangduy03 <dangduynguyen03@gmail.com>
2024-01-08 16:11:56 +0700

    add baitap

---
+++ js/vidu.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 30 ;
+var c = a+b;
+document.write ("Tong: a+b ="+c +"<br>" ) 
+var x =100+ 200;
+document.write ("Tong x la: " +x)
+
+// dien tich hinh chu nhat
+var a = prompt("Nhap chieu dai HCN:","100" )
+var b = prompt("Nhap chieu rong HCN:","200" )
+alert ("Dien tich HCN la : "+ a*b)
